6486592379e1ade86740d1463ceab7e4b020d6c9
Peter Sayaseng <pasayaseng@yahoo.com>
2023-11-16 01:59:51 +0000

    Resolve "Create GET user endpoint"

--- api/queries/users.py
+++ api/queries/users.py
@@ -1,5 +1,6 @@
+from unittest import result
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from queries.pool import pool
 
 
@@ -19,6 +20,33 @@ class UserOut(BaseModel):
 
 
 class UserRepository:
+    def get_all_users(self) -> List[UserOut]:
+        try:
+            with pool.connection() as conn:
+
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, username, email, password, profile_picture
+                        FROM users
+                        ORDER BY username
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        users = UserOut(
+                            id=record[0],
+                            username=record[1],
+                            email=record[2],
+                            password=record[3],
+                            profile_picture=record[4],
+                        )
+                        result.append(users)
+                    return result
+        except Exception as e:
+            return {"message":"Could not get all users"}
+
+
     def create(self, users: UserIn) -> UserOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, Depends
-from queries.users import UserIn, UserRepository
-
+from queries.users import UserIn, UserOut, UserRepository
+from typing import List
 
 router = APIRouter()
 
 
-@router.post("/users")
+@router.post("/users", response_model=UserOut)
 def create_user(
     users: UserIn,
     repo: UserRepository = Depends()
 ):
     return repo.create(users)
+
+@router.get("/users", response_model=List[UserOut])
+def get_all_users(
+    repo: UserRepository = Depends(),
+):
+    return repo.get_all_users()

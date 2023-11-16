7cc3a3ad392acc22fcae444ff8b82746756c0ba8
J Melendrez <jmelendrez5640@gmail.com>
2023-11-16 23:16:22 +0000

    Resolve "Backend endpoint to GET all games"

--- api/queries/games.py
+++ api/queries/games.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from queries.pool import pool
 from datetime import date
+from typing import List
 
 
 class GameIn(BaseModel):
@@ -37,3 +38,24 @@ class GameRepository:
                 id = result.fetchone()[0]
                 old_data = games.dict()
                 return GameOut(id=id, **old_data)
+    def get_all(self) -> List[GameOut]:
+        with pool.connection() as conn:
+
+            with conn.cursor() as db:
+
+                result = db.execute(
+                    """
+                    SELECT id, title, release_date, esrb_rating
+                    FROM games
+                    ORDER BY title
+                    """
+                )
+                return [
+                    GameOut(
+                    id=record[0],
+                    title=record[1],
+                    release_date=record[2],
+                    esrb_rating=record[3]
+                    )
+                for record in db
+               ]

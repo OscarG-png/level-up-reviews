58126aaa97769b02a7af1960041853cdfdae71d1
Oscar Gamez <oscar.gamez88@gmail.com>
2023-12-05 01:56:09 +0000

    Feature/genre games

---
+++ api/migrations/004_db.py
@@ -0,0 +1,366 @@
+steps = [
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (1,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (2,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (3,
+        7)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (4,
+        2)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (5,
+        6)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (6,
+        4)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (7,
+        3)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (8,
+        8)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (9,
+        2)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (10,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (11,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (12,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (13,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (14,
+        3)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (15,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (16,
+        2)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (17,
+        3)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (18,
+        10)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (19,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (20,
+        11)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (21,
+        9)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (22,
+        4)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (23,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (24,
+        2)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (25,
+        5)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (26,
+        1)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (27,
+        4)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+    [
+        """
+        INSERT INTO game_genres
+        (game_id,
+        genre_id)
+        VALUES
+        (28,
+        12)
+        """,
+        """
+        DROP TABLE game_genres;
+        """,
+    ],
+]

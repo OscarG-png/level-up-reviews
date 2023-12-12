70b00240cab73228cbe4e2198c0df74d9b1c672f
James Nguyen <jamesnguyenthe1st@gmail.com>
2023-12-12 15:17:04 -0800

    css

--- ghi/src/reviews/CreateReviews.js
+++ ghi/src/reviews/CreateReviews.js
@@ -51,7 +51,7 @@ function CreateReview({ userData }) {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 dark:bg-gray-800 text-black dark:text-white">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Create a Review
